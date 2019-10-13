52d9121febcd99633031477c57e07b1522d06f4b
Alexander Rubenov <a.rubenov@flatplan.design>
2019-10-13 22:16:14 +0300

    🐣

---
+++ src/context/alert/alertReducer.js
@@ -0,0 +1,13 @@
+import { SHOW_ALERT } from '../types.js';
+import { HIDE_ALERT } from '../types.js';
+
+const handlers = {
+    [SHOW_ALERT]: (state, {payload}) => ({...payload, visible: true}),
+    [HIDE_ALERT]: state => ({...state, visible: false}),
+    DEFAULT: state => state,
+};
+
+export const alertReducer = (state, action) => {
+    const handle = handlers[action.type] || handlers.DEFAULT;
+    return handle(state, action);
+};
---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">
+                Лучшее React-приложение
+            </h1>
+            <p className="lead">
+                Создано с <strong>♥</strong> by Alexander
+            </p>
+        </div>
+    </div>
+);
